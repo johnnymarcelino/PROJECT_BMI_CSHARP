544ddac36b6946e0b56ec4906e62bafa4a330903
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-07-13 18:49:49 -0300

    [Task: Project - BMI (Body Mass Index)] {13/07/2022 18:48 hour - BR

---
+++ IMC/IMC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IMC {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com o peso: ");
+            float peso = (float) Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Entre com a altura"+ peso);
+            float altura = (float) Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Peso da pessoa: " + peso);
+            Console.WriteLine($"Altura da pessoa: {altura}");
+
+            float result = peso / altura * altura;
+
+            if (result < 18) {
+                Console.WriteLine("Magrinho");
+            }
+            else if (result > 18 && result < 25) {
+                Console.WriteLine("Normal");
+            } 
+
+        }
+    }
+}
